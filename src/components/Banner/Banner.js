34bb8551ac82011048c161415dab5a3de5356e8b
A.K Bhat <g.ananthakrishnabhat@gmail.com>
2024-11-26 17:50:49 +0530

    Banner setup done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,20 +1,35 @@
-import React from 'react'
-import './Banner.css'
+import React, { useEffect, useState } from "react";
+import { API_KEY ,imageUrl} from "../../constants/constants";
+import "./Banner.css";
+import axios from '../../axios';
 function Banner() {
-    return (
-        <div 
-         className='banner'>
-            <div className='content' >
-                <h1 className='title'>Money  </h1>
-                <div className='banner_buttons' >
-                    <button className='button' >Play</button>
-                    <button className='button' >My list</button>
-                </div>
-                <h1 className='description'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content</h1>
-            </div>
-        <div className="fade_bottom"></div>
+  const [movie, setMovie] = useState();
+  
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((res) => {
+      const randomIndex = Math.floor(Math.random() * 19);
+      
+      setMovie(res.data.results[randomIndex]);
+    })
+
+},[])
+
+  return (
+    <div
+      className="banner"
+      style={{ backgroundImage:` url(${imageUrl + movie?.backdrop_path })`}}
+    >
+      <div className="content">
+        <h1 className="title">{movie?.title}</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My list</button>
         </div>
-    )
+        <h1 className="description">{movie?.overview}</h1>
+      </div>
+      <div className="fade_bottom"></div>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = "https://api.themoviedb.org/3";
+export const API_KEY = "e13dba65b7c7698096c1acb9475ceeea"; 
+export const imageUrl = "https://image.tmdb.org/t/p/original";
